36a76bcb28ca17d8ed2a4d832a52efabc8511a3a
Putra <syahdilla.ps@gmail.com>
2025-03-19 14:17:48 +0700

    🔄 Refactor(routing): Change greeting endpoint and add a new endpoint to persist a DB field.

--- src/main/java/io/welearn/GreetingResource.java
+++ src/main/java/io/welearn/GreetingResource.java
@@ -1,16 +1,30 @@
 package io.welearn;
 
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
+    @Path("/hello-world")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    @GET
+    @Path("/db-field")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Transactional
+    public String dbField() {
+        MyEntity entity = new MyEntity();
+        entity.field = "field-1";
+        entity.persist();
+        return "total data: " + MyEntity.count();
+    }
+
 }
